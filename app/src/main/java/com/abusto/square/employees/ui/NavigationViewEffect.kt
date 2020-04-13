ecca104b5c427e9007becc3a90ae4a5556e8810b
Anthony <anthonybusto22@gmail.com>
2020-04-13 13:47:46 -0400

    add view effects and ReducerResult

--- app/src/main/java/com/abusto/square/employees/ui/NavigationViewEffect.kt
+++ app/src/main/java/com/abusto/square/employees/ui/NavigationViewEffect.kt
@@ -1,5 +1,7 @@
 package com.abusto.square.employees.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.abusto.square.base_arch.ViewEffect
 
@@ -7,5 +9,9 @@ data class NavigationViewEffect(private val destination: Class<*>,
                                 private val args: Bundle? = null) : ViewEffect<Pair<Class<*>, Bundle?>>() {
 
     override val value: Pair<Class<*>, Bundle?> = destination to args
+
+    fun execute(context: Context) = consume { ( destination, bundle) ->
+        context.startActivity(Intent(context, destination), bundle)
+    }
 
 }
